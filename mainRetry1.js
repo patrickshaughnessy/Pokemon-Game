b57eb4fce7429375422b301ce91470322daafb9c
Patrick Shaughnessy <pshaughn86@gmail.com>
2015-11-05 22:48:55 -0800

    remove https calls from pokeapi

--- mainRetry1.js
+++ mainRetry1.js
@@ -30,7 +30,7 @@
   function generatePokemon() {
     let pokedex;
 
-    $.get('https://pokeapi.co/api/v1/pokedex/1/')
+    $.get('http://pokeapi.co/api/v1/pokedex/1/')
       .done(function(data){
         pokedex = data;
             // var intervalID = window.setInterval(function(){
@@ -45,7 +45,7 @@
         let randomPokemonNumber = Math.floor(Math.random() * 778);
         let randomPokemonName = pokedex.pokemon[randomPokemonNumber].name;
 
-        let pokemonData = 'https://pokeapi.co/' + pokedex.pokemon[randomPokemonNumber].resource_uri;
+        let pokemonData = 'http://pokeapi.co/' + pokedex.pokemon[randomPokemonNumber].resource_uri;
 
         $.get(pokemonData)
           .done(function(data){
@@ -54,11 +54,11 @@
               // let $defense = $('<p>').text('Defense Power: ' + data.defense);
               // let $hp = $('<p>').text('HP: ' + data.hp);
 
-          let pokemonSprite = 'https://pokeapi.co/' + data.sprites[0].resource_uri;
+          let pokemonSprite = 'http://pokeapi.co/' + data.sprites[0].resource_uri;
 
           $.get(pokemonSprite)
             .done(function(data){
-              let pokemonImage = 'https://pokeapi.co/' + data.image;
+              let pokemonImage = 'http://pokeapi.co/' + data.image;
               let pokemonLocation = Math.floor(Math.random() * 120);
 
               pokemonRef.child(randomPokemonName).set({
